4870efee5f4bf2aa22230b9c82c775e8d782c43c
softfault <givenyoung46@gmail.com>
2026-03-15 08:05:23 +0800

    fix(parser): allow discard identifier `_` in match arm bindings

--- compiler/src/parser/mod.rs
+++ compiler/src/parser/mod.rs
@@ -1758,11 +1758,18 @@ impl<'a> Parser<'a> {
                     }
                 }
 
-                // 判断是否有数据绑定: `: val`
+               // 判断是否有数据绑定: `: val`
                 let mut binding = None;
                 if self.match_token(&[TokenType::Colon]) {
-                    let bind_token = self.expect(TokenType::Identifier)?;
-                    binding = Some(self.intern_token(bind_token));
+                    // 为丢弃符开绿灯
+                    if self.match_token(&[TokenType::Underscore]) {
+                        // 把它统一转换成名为 "_" 的符号存入 AST
+                        binding = Some(self.context.intern("_")); 
+                    } else {
+                        // 否则走正常的标识符解析
+                        let bind_token = self.expect(TokenType::Identifier)?;
+                        binding = Some(self.intern_token(bind_token));
+                    }
                 }
 
                 let pat_span = arm_start.to(self.stream.prev_span());
